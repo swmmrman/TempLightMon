d39e4d93bf6e496a0406559ff0ae7e0e8c9a4c5a
Stephan Kern <stephan@swmmrman.com>
2021-06-07 06:27:45 -0600

    "Starting" point

---
+++ main.py
@@ -0,0 +1,28 @@
+import logic
+import serial
+
+ser = serial.Serial('/dev/ttyUSB0', 115200)
+print("Starting")
+(temp1, temp2, ldr) = ser.readline().decode('utf-8').strip().split(",")
+maxt1 = float(temp1)
+mint1 = float(temp1)
+maxt2 = float(temp2)
+mint2 = float(temp2)
+maxldr = int(ldr)
+minldr = int(ldr)
+print("\n\n")
+while True:
+    (temp1, temp2, ldr) = ser.readline().decode('utf-8').strip().split(",")
+    temp1 = float(temp1)
+    temp2 = float(temp2)
+    ldr = int(ldr)
+    if minldr > ldr: minldr = ldr
+    if maxldr < ldr: maxldr = ldr
+    if maxt1 < temp1: maxt1 = temp1
+    if mint1 > mint1: mint1 = temp1
+    if maxt2 < temp2: maxt2 = temp2
+    if mint2 > temp2: maxt2 = temp2
+    ups = "\033[1A"
+    print(F"{ups * 3}Current: \tTemp 1: {temp1:3.2f} Temp2: {temp2:3.2f} LDR: {ldr:04d}")
+    print(F"Maximums: \tTemp 1: {maxt1:3.2f} Temp2: {maxt2} LDR: {maxldr:04d}")
+    print(F"Minimums: \tTemp 1: {mint1:3.2f} Temp2: {mint2} LDR: {minldr:04d}")
