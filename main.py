8551ba8c5b6372091381e54e2026775519cf9c0b
Stephan Kern <stephan@swmmrman.com>
2021-06-19 16:02:18 -0600

    Convert LDR reading back to int.  And print out humidity

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ print("Starting")
 maxt1 = mint1 = temp1
 maxh1 = minh1 = h1
 maxt2 = mint2 = temp2
+maxh2 = minh2 = h2
 maxd = mind = temp1 - temp2
 
 maxldr = int(ldr)
@@ -15,6 +16,7 @@ print("\n\n")
 while True:
     (temp1, temp2, h1, h2, ldr) = map(float, ser.readline().decode('utf-8').strip().split(","))
     delta = temp1 - temp2
+    ldr = int(ldr)
     if minldr > ldr: minldr = ldr
     if maxldr < ldr: maxldr = ldr
     if maxt1 < temp1: maxt1 = temp1
@@ -28,6 +30,6 @@ while True:
     if maxd < delta: maxd = delta
     if mind > delta: mind = delta
     ups = "\033[1A"
-    print(F"{ups * 3}Current: \tTemp 1: {temp1:3.2f} Temp2: {temp2:3.2f} Delta: {delta:05.2f} LDR: {ldr:04d}")
+    print(F"{ups * 3}Current: \tTemp 1: {temp1:3.2f} Humidity1: {h1:5.2f} Temp2: {temp2:3.2f} Humidity2: {h2:5.2f} Delta: {delta:05.2f} LDR: {ldr:04d}")
     print(F"Maximums: \tTemp 1: {maxt1:3.2f} Temp2: {maxt2:3.2f} Delta: {maxd:05.2f} LDR: {maxldr:04d}")
     print(F"Minimums: \tTemp 1: {mint1:3.2f} Temp2: {mint2:3.2f} Detla: {mind:05.2f} LDR: {minldr:04d}")
