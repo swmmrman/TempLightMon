d270e47725d05edc40a2a6c81095dfcab85e5802
Stephan Kern <stephan@swmmrman.com>
2021-06-19 20:43:54 -0600

    More formating

--- main.py
+++ main.py
@@ -31,6 +31,6 @@ while True:
     if mind > delta: mind = delta
     ups = "\033[1A"
     print(F"{ups * 3}", end="")
-    print(F"Current: \tT1: {temp1:5.2f} H1: {h1:5.2f} T2: {temp2:5.2f} H2: {h2:5.2f} TD: {delta:05.2f} LDR: {ldr:04d}")
-    print(F"Maximums: \tT1: {maxt1:5.2f} H1: {maxh1:5.2f} T2: {maxt2:5.2f} H2: {maxh2:5.2f} TD: {maxd:05.2f} LDR: {maxldr:04d}")
-    print(F"Minimums: \tT1: {mint1:5.2f} H1: {minh1:5.2f} T2: {mint2:5.2f} H2: {minh2:5.2f} TD: {mind:05.2f} LDR: {minldr:04d}")
+    print(F"Current: \tT1: {temp1:5.2f} H1: {h1: 5.2f} T2: {temp2:5.2f} H2: {h2: 5.2f} TD: {delta: 5.2f} LDR: {ldr:04d}")
+    print(F"Maximums: \tT1: {maxt1:5.2f} H1: {maxh1: 5.2f} T2: {maxt2:5.2f} H2: {maxh2: 5.2f} TD: {maxd: 5.2f} LDR: {maxldr:04d}")
+    print(F"Minimums: \tT1: {mint1:5.2f} H1: {minh1: 5.2f} T2: {mint2:5.2f} H2: {minh2: 5.2f} TD: {mind: 5.2f} LDR: {minldr:04d}")
