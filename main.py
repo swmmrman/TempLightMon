1bc32de489293c2b1df25abd57503fe057ee306e
Stephan Kern <stephan@swmmrman.com>
2021-06-19 20:29:13 -0600

    Fix missing " and change how td is calculated.

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ maxt1 = mint1 = temp1
 maxh1 = minh1 = h1
 maxt2 = mint2 = temp2
 maxh2 = minh2 = h2
-maxd = mind = temp1 - temp2
+maxd = mind = temp2 - temp1
 
 maxldr = int(ldr)
 minldr = int(ldr)
@@ -30,7 +30,7 @@ while True:
     if maxd < delta: maxd = delta
     if mind > delta: mind = delta
     ups = "\033[1A"
-    print(F"{ups * 3}, end="")
+    print(F"{ups * 3}", end="")
     print(F"Current: \tT1: {temp1:5.2f} H1: {h1:5.2f} T2: {temp2:5.2f} H2: {h2:5.2f} TD: {delta:05.2f} LDR: {ldr:04d}")
     print(F"Maximums: \tT1: {maxt1:5.2f} H1: {maxh1:5.2f} T2: {maxt2:5.2f} H2: {maxh2:5.2f} TD: {maxd:05.2f} LDR: {maxldr:04d}")
     print(F"Minimums: \tT1: {mint1:5.2f} H1: {minh1:5.2f} T2: {mint2:5.2f} H2: {minh2:5.2f} TD: {mind:05.2f} LDR: {minldr:04d}")
