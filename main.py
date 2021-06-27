00848a72420c7524b1a7ac5b1b48b61432b9a64a
Stephan Kern <stephan@swmmrman.com>
2021-06-27 04:36:35 -0600

    Adding TD arrows, and tweaking formating

--- main.py
+++ main.py
@@ -10,12 +10,17 @@ maxt2 = mint2 = temp2
 maxh2 = minh2 = h2
 maxd = mind = temp2 - temp1
 
+up_arrow = u'\u2191'
+down_arrow = u'\u2193'
+
 maxldr = int(ldr)
 minldr = int(ldr)
 print("\n\n")
 while True:
     (temp1, temp2, h1, h2, ldr) = map(float, ser.readline().decode('utf-8').strip().split(","))
     delta = temp2 - temp1
+    dv = abs(delta)
+    arrow = up_arrow if delta > 0 else down_arrow
     ldr = int(ldr)
     if minldr > ldr: minldr = ldr
     if maxldr < ldr: maxldr = ldr
@@ -29,8 +34,12 @@ while True:
     if maxh2 < h2: maxh2 = h2
     if maxd < delta: maxd = delta
     if mind > delta: mind = delta
+    hdv = abs(maxd)
+    ldv = abs(mind)
+    harrow = up_arrow if maxd > 0 else down_arrow
+    larrow = up_arrow if mind > 0 else down_arrow
     ups = "\033[1A"
     print(F"{ups * 3}", end="")
-    print(F"Current: \tT1: {temp1:5.2f} H1: {h1: 5.2f} T2: {temp2:5.2f} H2: {h2: 5.2f} TD: {delta: 5.2f} LDR: {ldr:04d}")
-    print(F"Maximums: \tT1: {maxt1:5.2f} H1: {maxh1: 5.2f} T2: {maxt2:5.2f} H2: {maxh2: 5.2f} TD: {maxd: 5.2f} LDR: {maxldr:04d}")
-    print(F"Minimums: \tT1: {mint1:5.2f} H1: {minh1: 5.2f} T2: {mint2:5.2f} H2: {minh2: 5.2f} TD: {mind: 5.2f} LDR: {minldr:04d}")
+    print(F'Current: \tT1: {temp1:5.2f} H1: {h1: 5.2f} T2: {temp2:5.2f} H2: {h2: 5.2f} TD: {dv: 6.2f}{arrow} LDR: {ldr:04d}')
+    print(F"High:\t\tT1: {maxt1:5.2f} H1: {maxh1: 5.2f} T2: {maxt2:5.2f} H2: {maxh2: 5.2f} TD: {hdv: 6.2f}{harrow} LDR: {maxldr:04d}")
+    print(F"Low:\t\tT1: {mint1:5.2f} H1: {minh1: 5.2f} T2: {mint2:5.2f} H2: {minh2: 5.2f} TD: {ldv: 6.2f}{larrow} LDR: {minldr:04d}")
